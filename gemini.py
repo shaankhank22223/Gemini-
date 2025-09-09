26a5b24976b0a9fafa55a7215af8fb1b5f6de3b7
Shaan Khan K <pip000470@gmail.com>
2025-09-09 06:41:22 +0500

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ðŸ”¹ Gemini API Key
-genai.configure(api_key=os.getenv("AIzaSyCrEPz_Dg-b57leqNxlGiJEOuC6r_YM6Ik"))
+genai.configure(api_key=os.getenv("AIzaSyAfHjiVK4A5RLl0wXs-ZAJ31tbAOCs_gc4"))
 
 # ðŸ”¹ Permanent System Prompt
 system_prompt = """
